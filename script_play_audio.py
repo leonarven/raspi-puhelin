3105eb522537378ec160ad16b7c85d7490c93ce1
Miro Nieminen <leonarven@gmail.com>
2024-02-13 10:08:10 +0200

    Lisätty lammas.mp3'a soittava skripti

---
+++ script_play_audio.py
@@ -0,0 +1,13 @@
+import audio
+import const
+
+def main():
+	audio.playAudio( const.AUDIO_FILE_LAMMAS )
+
+if __name__ == '__main__':
+        try:
+                main()
+        except KeyboardInterrupt:
+                debug("Ohjelma lopetettu")
+                GPIO.cleanup()
+                sys.exit()
