b3297da372d3aa10e3082a81bec6646ade81b607
Miro Nieminen <leonarven@gmail.com>
2024-02-13 09:43:00 +0200

    Lisätty luokat KeypadPinger ja KeypadHandler

---
+++ lib/KeypadHandler.py
@@ -0,0 +1,146 @@
+from const import AUDIO_PAD_SEQ_ORDER
+
+class KeypadHandler:
+
+	def _c_r_2_pad( self, c, r ):
+		n = 3 * r + c
+		return AUDIO_PAD_SEQ_ORDER[ n ] if n < 12 else None
+
+	_multikey_fix_mapping = {
+		'1A': '1',
+		'12': '2',
+		'13': '3',
+		'4A': '4',
+		'45': '5',
+		'46': '6',
+		'7A': '7',
+		'78': '8',
+		'79': '9',
+		'A0': '0',
+		'AB': 'B',
+	}
+
+	def _solve_multikey_fix( self, keys ):
+		key_s = ''.join( keys )
+		return self._multikey_fix_mapping[ key_s ] if key_s in self._multikey_fix_mapping else None
+
+	# --------------------
+
+	iteration = 0
+
+	pinger = None
+
+	def __init__( self, pinger ):
+		self.pinger = pinger;
+		pass
+
+
+	keydetection_listener = None
+
+	def registerKeydetectionListener( self, listener ):
+		self.keydetection_listener = listener
+
+	keypressed_listener = None
+
+	def registerKeypressedListener( self, listener ):
+		self.keypressed_listener = listener
+	
+	keydown_listener = None
+
+	def registerKeydownListener( self, listener ):
+		self.keydown_listener = listener
+
+	keyup_listener = None
+
+	def registerKeyupListener( self, listener ):
+		self.keyup_listener = listener
+
+
+	def iterate( self ):
+		
+		self.iteration += 1
+
+		keys_by_pins = self.pinger.iterateActiveKey()
+		keys = []
+
+		for col_idx, rows in enumerate( keys_by_pins ):
+			for row_idx in rows:
+				keys.append( self._c_r_2_pad( col_idx, row_idx ) )
+
+		# Ei painallusta
+		if len(keys) == 0:
+			self.onKeyDetected( None )
+			return
+
+		# Mahdollinen virhepainallus (tai monen napin painallus)
+		if len(keys) > 1:
+			fix = self._solve_multikey_fix( keys )
+			if fix is not None:
+				keys = [ fix ]
+
+		# Lopulta hallussa vain lopulliset painallukset yksi painallus!
+		if len(keys) == 1:
+
+			if keys[0] is not None:
+
+				#print( "SUCCESS :: ", keys[0] )
+
+				self.onKeyDetected( keys[0] )
+
+			else:
+				print( "ERROR_A  ::   ", keys )
+				
+		else:
+			print( "ERROR_B ::   ", keys )
+	
+
+
+	prev_keys_count = 3
+	prev_keys = [ None, None, None]
+
+	def onKeyDetected( self, key ):
+		if self.keydetection_listener is not None:
+			self.keydetection_listener( key )
+
+		self.prev_keys.append( key )
+		self.prev_keys = self.prev_keys[-self.prev_keys_count:]
+
+		sum_key = key
+
+		for k in self.prev_keys:
+			if k != sum_key:
+				return
+
+		self.onKeyPressed( key )
+			
+
+	pressed_key = None
+
+	def onKeyPressed( self, key ):
+		if self.keypressed_listener is not None:
+			self.keypressed_listener( key )
+
+		if self.pressed_key == key: return
+
+		prev_key = self.pressed_key
+
+		self.pressed_key = key
+
+		if key is None: # Nappia ei ole enää painettu!
+			self.onKeyup( prev_key )
+
+		elif prev_key is not None: # Äsken oli eri nappi pohjassa
+			self.onKeyup( prev_key )
+			self.onKeydown( key )
+
+		else: # Ei ollut äsken mitään pohjassa
+			self.onKeydown( key )
+
+
+	def onKeyup( self, key ):
+		if self.keyup_listener is not None:
+			self.keyup_listener( key )
+
+	def onKeydown( self, key ):
+		if self.keydown_listener is not None:
+			self.keydown_listener( key )
---
+++ lib/KeypadPinger.py
@@ -0,0 +1,85 @@
+import const 
+import time
+import Switch
+
+from GPIO import GPIO
+
+
+class KeypadPinger: 
+
+    delay_counter = 0
+
+    row_pins = [ None, None, None, None ]
+    col_pins = [ None, None, None ]
+
+    cols_state = [ GPIO.LOW, GPIO.LOW, GPIO.LOW ]
+
+    def __init__( self, row_pins, col_pins ):
+        self.row_pins = row_pins
+        self.col_pins = col_pins
+
+        [ GPIO.setup(  pin, GPIO.OUT ) for pin in col_pins ]
+        [ GPIO.output( pin, GPIO.LOW ) for pin in col_pins ]
+
+        self.switchs = [
+            Switch.Switch( row_pins[3], "pin_" + str(const.PIN_KEYBOARD_5) + " / pad_5 / R4" ),
+            Switch.Switch( row_pins[2], "pin_" + str(const.PIN_KEYBOARD_7) + " / pad_7 / R3" ),
+            Switch.Switch( row_pins[1], "pin_" + str(const.PIN_KEYBOARD_8) + " / pad_8 / R2" ),
+            Switch.Switch( row_pins[0], "pin_" + str(const.PIN_KEYBOARD_9) + " / pad_9 / R1" )
+        ]
+
+    
+    def __delay( self ):
+        self.delay_counter += 1
+        #print("[" , str(self.delay_counter).zfill(7) , "]")
+        time.sleep( const.MS_10*5 )
+
+    def __apply( self ):
+        GPIO.output( self.col_pins[0], self.cols_state[0] )
+        GPIO.output( self.col_pins[1], self.cols_state[1] )
+        GPIO.output( self.col_pins[2], self.cols_state[2] )
+
+    def __clear( self ):
+        self.cols_state = [ GPIO.LOW, GPIO.LOW, GPIO.LOW ]
+
+    def clear( self ):
+        self.__clear()
+        self.__apply()
+        self.__delay()
+
+    def apply( self ):
+
+        self.__clear()
+
+        if self.active_col is not None:
+            self.cols_state[ self.active_col ] = GPIO.HIGH
+
+        self.__apply()
+        self.__delay()
+
+    def activateCol( self, col ):
+        self.active_col = col;
+        self.apply()
+
+
+    def readCol( self ):
+        return [
+            GPIO.input( self.row_pins[0] ),
+            GPIO.input( self.row_pins[1] ),
+            GPIO.input( self.row_pins[2] ),
+            GPIO.input( self.row_pins[3] )
+        ]
+    
+    def readActiveRows( self ):
+        return [ i for i, v in enumerate( self.readCol() ) if v == GPIO.HIGH ]
+    
+    def iterateActiveKey( self ):
+
+        self.activateCol( 0 )
+        r1 = self.readActiveRows()
+        self.activateCol( 1 )
+        r2 = self.readActiveRows()
+        self.activateCol( 2 )
+        r3 = self.readActiveRows()
+
+        return [ r1, r2, r3 ]
