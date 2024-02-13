4b6b6ced8b515c29756fdcccfcc1a2ed8df0e1a3
Miro Nieminen <leonarven@gmail.com>
2024-02-13 10:07:36 +0200

    Siirretty vakioita const.py'hn

--- script_keypad_pinger.py
+++ script_keypad_pinger.py
@@ -12,18 +12,7 @@ import KeypadHandler
 
 # ----------------
 
-PIN_C1 = const.PIN_KEYBOARD_3
-PIN_C2 = const.PIN_KEYBOARD_4
-PIN_C3 = const.PIN_KEYBOARD_6
-
-PIN_R1 = const.PIN_KEYBOARD_9
-PIN_R2 = const.PIN_KEYBOARD_8
-PIN_R3 = const.PIN_KEYBOARD_7
-PIN_R4 = const.PIN_KEYBOARD_5
-
-# ----------------
-
-pinger = KeypadPinger.KeypadPinger([ PIN_R1, PIN_R2, PIN_R3, PIN_R4 ], [ PIN_C1, PIN_C2, PIN_C3 ]);
+pinger = KeypadPinger.KeypadPinger( const.KEYPAD_ROWS_PINS, const.KEYPAD_COLS_PINS )
 keypad = KeypadHandler.KeypadHandler( pinger )
 
 sound = audio.getOrGenerateAudio( const.AUDIO_FILE_PAD )
